7bd2dcb6661bf4e73b4e9491c06ded94741b9f0f
Wesley <wesleys.felipe@gmail.com>
2015-04-10 14:23:36 -0300

    Html e css finalizados para tela de cadastro

---
+++ assets/js/cadastro-usuario.js
@@ -0,0 +1,15 @@
+/*
+ * Core script to handle all cadastro-usuario specific things
+ */
+
+var Cadastro = function () {
+
+    "use strict";
+
+    return {
+        // main function to initiate all plugins
+        init: function () {
+            datePick();
+        },
+    };
+}();
