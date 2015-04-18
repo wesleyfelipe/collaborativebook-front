d5ab2e1136a361578e9c4a3cea1d5cf9d6a8fbff
Wesley <wesleys.felipe@gmail.com>
2015-04-17 21:39:10 -0300

    Lista estática de gêneros literários para criação de livro

---
+++ assets/js/novo-livro.js
@@ -0,0 +1,30 @@
+/*
+ * Author: Wesley Felipe da Silva
+ */
+
+var NovoLivro = function () {
+
+    "use strict";
+    
+    var generos = ["Humor","Ficção","Romance","Poesia","Mistério","Terror","Drama","Ficção científica",
+                    "Infantil","Juvenil","Fantasia","Aventura","Policial","Espiritualidade/Religião",
+                    "História","Crônicas","Mitologia","Distopia"]
+    
+    //funções
+    var listarGeneros = function () { 
+        //capitulos
+        for (var i = 0; i < generos.length; i++) {
+            $('select#genero').append("<option>"+generos[i]+"</option>");
+        }
+    };
+
+    return {
+        init: function () {
+            $(".nomeusuario").append(localStorage.getItem("username"));
+            $('select').select2();
+            
+            listarGeneros();
+        }
+    };
+
+}();
