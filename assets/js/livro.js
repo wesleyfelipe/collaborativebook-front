593cd64e6327a7ee46d07385fc69a8d7273190fc
Wesley <wesleys.felipe@gmail.com>
2015-06-27 23:23:14 -0300

    Exibindo capítulos

--- assets/js/livro.js
+++ assets/js/livro.js
@@ -17,22 +17,42 @@ var Livro = function () {
     var capAp2 = {titulo: "Epílogo", texto: "1...2...3...", autor: "Machado de Assis"};
     var capitulosAprovacaoLivro = [capAp1, capAp2];
 
-//    var livro = {proprietario: "Batman", titulo: "Sem criatividade", genero: "Crônica", enredo: "Não há",
-//        personagens: "Não há", ambientação: "Não há", capitulos: capitulosLivro, capitulosAprovacao: capitulosAprovacaoLivro};
-
-
     $.urlParam = function (sParam) {
         var sPageURL = window.location.search.substring(1);
         var sURLVariables = sPageURL.split('&');
-        for (var i = 0; i < sURLVariables.length; i++){
+        for (var i = 0; i < sURLVariables.length; i++) {
             var sParameterName = sURLVariables[i].split('=');
-            if (sParameterName[0] === sParam){
+            if (sParameterName[0] === sParam) {
                 return sParameterName[1];
             }
         }
     };
 
     //funções
+    var apresentarCapitulos = function (capitulos) {
+        for (var i in capitulos) {
+            if(capitulos[i].aprovado)
+                exibirCapitulo(capitulos[i]);
+            else
+                exibirCapituloNaoAprovado(capitulos[i]);
+        }
+    };
+
+    var exibirCapituloAprovado = function (capitulo) {
+        $('table#capitulos tr:last').after("<tr><td class='align-center'><ul class='table-controls'>\n\
+        <li><a href='capitulo.html' class='bs-tooltip' title='Visualizar'><i class='icon-search'></i></a> </li>\n\
+        <li><a href='javascript:void(0);' class='bs-tooltip' title='Deletar'><i class='icon-trash'></i></a> </li>\n\
+        </ul></td><td>" + capitulo.titulo + "</td><td>" + capitulo.autor + "</td></tr>");
+    };
+    
+    var exibirCapituloNaoAprovado = function(capitulo){
+        $('table#capitulos-para-aprovacao tr:last').after("<tr><td class='align-center'><ul class='table-controls'>\n\
+               <li><a href='capitulo.html' class='bs-tooltip' title='Visualizar'><i class='icon-search'></i></a> </li>\n\
+               <li><a href='javascript:void(0);' class='bs-tooltip' title='Deletar'><i class='icon-trash'></i></a> </li>\n\
+               <li><a href='javascript:void(0);' class='bs-tooltip' title='Aprovar'><i class='icon-check'></i></a> </li>\n\
+               </ul></td><td>" + capitulo.titulo + "</td><td>" + capitulo.autor + "</td></tr>");
+    };
+
     var apresentarLivro = function (livro) {
 
         $("#titulo").text(livro.titulo);
@@ -41,23 +61,33 @@ var Livro = function () {
         $("#enredo").text(livro.enredo);
         $("#personagens").text(livro.personagens);
         $("#ambientacao").text(livro.ambientação);
+    };
 
-//        //capitulos
-//        for (var i = 0; i < livro.capitulos.length; i++) {
-//            $('table#capitulos tr:last').after("<tr><td class='align-center'><ul class='table-controls'>\n\
-//                <li><a href='capitulo.html' class='bs-tooltip' title='Visualizar'><i class='icon-search'></i></a> </li>\n\
-//                <li><a href='javascript:void(0);' class='bs-tooltip' title='Deletar'><i class='icon-trash'></i></a> </li>\n\
-//                </ul></td><td>" + livro.capitulos[i].titulo + "</td><td>" + livro.capitulos[i].autor + "</td></tr>");
-//        }
-//
-//        //capitulos para aprovacao
-//        for (var i = 0; i < livro.capitulosAprovacao.length; i++) {
-//            $('table#capitulos-para-aprovacao tr:last').after("<tr><td class='align-center'><ul class='table-controls'>\n\
-//                <li><a href='capitulo.html' class='bs-tooltip' title='Visualizar'><i class='icon-search'></i></a> </li>\n\
-//                <li><a href='javascript:void(0);' class='bs-tooltip' title='Deletar'><i class='icon-trash'></i></a> </li>\n\
-//                <li><a href='javascript:void(0);' class='bs-tooltip' title='Aprovar'><i class='icon-check'></i></a> </li>\n\
-//                </ul></td><td>" + livro.capitulosAprovacao[i].titulo + "</td><td>" + livro.capitulosAprovacao[i].autor + "</td></tr>");
-//        }
+    var recuperarCapitulos = function () {
+        $.ajax({
+            url: "http://colaborativebook.herokuapp.com/api/livro/" + $.urlParam("idLivro") + "/capitulo",
+            type: 'get',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-Access-Token', sessionStorage.getItem("token"));
+            },
+            response: {
+                format: 'json'
+            },
+            statusCode: {
+                200: function (response) {
+                    apresentarCapitulos(response);
+                },
+                401: function (response) {
+                    window.location.href = "login.html";
+                },
+                404: function () {
+                    alert("Ocorreu um erro na sua requisição. Estamos trabalhando nesta correção.");
+                },
+                500: function () {
+                    alert("Ops! Algo errado aconteceu. Tente novamente daqui alguns instantes.");
+                }
+            }
+        });
     };
 
     var recuperarLivro = function () {
@@ -94,6 +124,7 @@ var Livro = function () {
             $('select').select2();
 
             recuperarLivro();
+            recuperarCapitulos();
         }
     };
 
